b56050d03e3a1085848fc8dc04c1f470d748f101
Usama <usamanasir464@gmail.com>
2022-07-20 05:33:45 -0700

    WorkingOnProductRepository

---
+++ MultiShop.Mvc.DataAccess/Infrastructure/IRepository/IProducts.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Mvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Mvc.DataAccess.Infrastructure.IRepository
+{
+   public interface IProducts
+    {
+        Task<List<Product>> GetAllProducts();
+    }
+}
---
+++ MultiShop.Mvc.DataAccess/Infrastructure/Repository/Products.cs
@@ -0,0 +1,35 @@
+﻿using MultiShop.Mvc.DataAccess.Infrastructure.IRepository;
+using MultiShop.Mvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Mvc.DataAccess.Infrastructure.Repository
+{
+    public class Products : IProducts
+    {
+        private readonly HttpClient _httpClient;
+
+        public Products( HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<List<Product>> GetAllProducts()
+        {
+            List<Product> products = new List<Product>();
+            _httpClient.BaseAddress = new Uri("https://localhost:44398/");
+            var response = await  _httpClient.GetAsync("ProductApi");
+            if (response.IsSuccessStatusCode)
+            {
+                var display =  response.Content.ReadAsStringAsync().Result;
+                products = JsonConvert.DeserializeObject<List<Product>>(display);
+
+            }
+        }
+    }
+}

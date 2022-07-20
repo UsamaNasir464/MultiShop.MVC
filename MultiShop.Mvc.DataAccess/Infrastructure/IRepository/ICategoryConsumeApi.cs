7e02e9d5289d702b507be6aa3cf545631e216bb6
Muhammad-Bilal <meharbilal24525@gmail.com>
2022-07-20 17:29:00 +0500

    CategoryConsumeApi GetAllCategory and GetCategoryById Work Done

---
+++ MultiShop.Mvc.DataAccess/Infrastructure/IRepository/ICategoryConsumeApi.cs
@@ -0,0 +1,15 @@
+﻿using MultiShop.Mvc.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Mvc.DataAccess.Infrastructure.IRepository
+{
+    public interface ICategoryConsumeApi
+    {
+        Task<List<Category>> GetAllCategory();
+    
+    }
+}
